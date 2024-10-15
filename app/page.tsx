6d55a92caa3217015cb72ddc57bf264d6180fe68
Andre Iasi Moura <amoura@destaxa.com.br>
2024-10-15 15:12:49 -0300

    lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import styles from "./styles.module.scss";
 import {Button, Link, TextField} from "@mui/material";
+import Image from "next/image";
 
 export default function Home() {
   return (
       <div className={styles.home}>
         <h1>
-          <div><img src="/images/logo.png" alt="Logo"/></div>
-          <div>Writer's Room</div>
+          <div>
+            <Image src="/images/logo.png" alt="Logo" width={173} height={161}/>
+          </div>
+          <div>Writer&apos;s Room</div>
         </h1>
         <div className="flex flex-col flex-grow justify-center items-center">
           <form className="flex flex-col">
